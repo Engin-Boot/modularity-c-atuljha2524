15332642335dd80247372f0b2d065e311d33a554
atuljha2524 <69622477+atuljha2524@users.noreply.github.com>
2020-09-01 10:59:13 +0530

    Update GetIndex.cs

--- TelCo.ColorCoder/GetIndex.cs
+++ TelCo.ColorCoder/GetIndex.cs
@@ -1,1 +1,21 @@
+using System;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace TelCo.ColorCoder
+{
+    partial class Program
+    {
+        private static int GetIndex(Color[] colorMap, Color color)
+        {
+            index = -1;
+            for (int i = 0; i < colorMap.length; i++) {
+                if (colorMap[i] == color) {
+                    return i;
+                }
+            }
+            return index;
+        }
+    }
+}
 
